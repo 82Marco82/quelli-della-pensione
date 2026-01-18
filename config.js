69b5c570a5ce72a45b8d01445c6f6d78111559e5
82Marco82 <82marcopalama@gmail.com>
2026-01-18 12:40:33 +0100

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,6 @@
+window.SUPABASE_URL = "https://cnvsqetaspqpbvkaiebx.supabase.co";
+window.SUPABASE_ANON_KEY = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImNudnNxZXRhc3BxcGJ2a2FpZWJ4Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg2NzY1MzEsImV4cCI6MjA4NDI1MjUzMX0.9N_gnCHQoR_NZ0YIlq0qmUQ9itnD_4jLE1ghXf78Kog";
 
+// compatibilità per tutti gli script
+var SUPABASE_URL = window.SUPABASE_URL;
+var SUPABASE_ANON_KEY = window.SUPABASE_ANON_KEY;
